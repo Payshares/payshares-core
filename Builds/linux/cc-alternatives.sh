20e8295b3bfa6d2fbaa426705124c40b0ef6c8dd
Marlon Hanks <owner@kedia.co>
2020-12-17 21:54:52 -0500

    Create cc-alternatives.sh

---
+++ Builds/linux/cc-alternatives.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Remove all existing alternatives
+sudo update-alternatives --remove-all cc
+
+# exit on first error
+set -e
+
+sudo update-alternatives \
+      --install /usr/bin/cc   cc   /usr/bin/gcc     30 \
+      --slave   /usr/bin/c++ c++ /usr/bin/g++  \
+      --slave   /usr/bin/ld ld /usr/bin/x86_64-linux-gnu-ld
+
+sudo update-alternatives \
+      --install /usr/bin/cc   cc   /usr/bin/clang     20 \
+      --slave   /usr/bin/c++ c++ /usr/bin/clang++  \
+      --slave   /usr/bin/ld ld /usr/bin/lld
